c46c253482cd6e966021087d36da5467aafeb2de
Rosemberg <rosemberc.g.02@gmail.com>
2022-05-14 13:36:52 -0500

    config server

---
+++ config-server/src/test/java/udc/practica/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package udc.practica.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
