61c3c449072365ff56547ca9919eb0567bd5c3c0
Rosemberg <rosemberc.g.02@gmail.com>
2022-05-15 18:53:28 -0500

    keycloak

---
+++ gateway-service/src/main/java/udc/practica/gatewayservice/security/SecurityController.java
@@ -0,0 +1,22 @@
+package udc.practica.gatewayservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityController {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http){
+        http.authorizeExchange().anyExchange().authenticated()
+                .and()
+                .oauth2Login(Customizer.withDefaults());
+        http.csrf().disable();
+        return http.build();
+    }
+}
